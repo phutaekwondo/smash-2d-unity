58138b04f1342e64fdec3542712e2f680e7de452
phutaekwondo <phutaekwondo2001@gmail.com>
2023-05-10 22:45:55 +0700

    keyboard to hit

--- Assets/Scripts/Input/InputKeyboardMouse.cs
+++ Assets/Scripts/Input/InputKeyboardMouse.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class InputKeyboardMouse : InputProvider
 {
+    private KeyCode m_firstKey = KeyCode.Z;
+    private KeyCode m_secondKey = KeyCode.X;
     public override Vector2? GetPressedPosition()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(m_firstKey) || Input.GetKeyDown(m_secondKey))
         {
             return Input.mousePosition;
         }
